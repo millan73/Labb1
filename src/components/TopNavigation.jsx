3bab6e63ab0557041a790677227d55d3c23feaad
Camilla Vedin <camilla.vedin@iths.se>
2023-12-11 20:45:51 +0100

    Added NotFound page with redirection to Startpage

--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import { navItems } from '../data/navItems'
-import { Link, Route, Routes } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import Start from '../pages/Start'
 import Bowling from '../pages/Bowling'
 import Biljard from '../pages/Biljard'
 import Dart from '../pages/Dart'
 import Contact from '../pages/Contact'
+import NotFound from '../pages/NotFound'
 
 function TopNavigation() {
     let topNavItem = []
@@ -22,9 +23,17 @@ function TopNavigation() {
                 <ul className="nav bg-dark">
                     {topNavItem.map((items) => (
                         <li key={items.id} className="nav-item">
-                            <Link to={items.route} className="nav-link">
+                            <NavLink
+                                style={({ isActive }) => {
+                                    {
+                                        return isActive ? { color: '#fff' } : {}
+                                    }
+                                }}
+                                to={items.route}
+                                className="nav-link"
+                            >
                                 {items.namn}
-                            </Link>
+                            </NavLink>
                         </li>
                     ))}
                 </ul>
@@ -35,6 +44,7 @@ function TopNavigation() {
                 <Route path="/biljard" element={<Biljard />} />
                 <Route path="/dart" element={<Dart />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     )
